950ded0584dbd37de3a8994f71f804e3732e5cdf
Siddharth Karn <siddharthkarn1204@gmail.com>
2026-03-30 05:09:40 +0530

    fix: cors for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,13 @@ const resumeRoutes = require("./routes/resumeRoutes");
 //const { connectDB } = require("./config/db");
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
 
